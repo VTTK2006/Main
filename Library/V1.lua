e4a329d3787307cab395179ff27c1f4ce0e2fa2c
Tuấn Khải <117251417+VTTK2006@users.noreply.github.com>
2026-06-21 14:57:03 +0700

    Update V1.lua

--- Library/V1.lua
+++ Library/V1.lua
@@ -178,7 +178,110 @@ Open.Visible=false
 
 end)
 
+--// DRAG MENU + LOGO
 
+
+local function MakeDrag(obj)
+
+
+local dragging=false
+
+local dragStart
+
+local startPos
+
+
+
+obj.InputBegan:Connect(function(input)
+
+if input.UserInputType ==
+Enum.UserInputType.MouseButton1 then
+
+
+dragging=true
+
+dragStart=input.Position
+
+startPos=obj.Position
+
+
+
+input.Changed:Connect(function()
+
+
+if input.UserInputState ==
+Enum.UserInputState.End then
+
+
+dragging=false
+
+
+end
+
+
+end)
+
+
+
+end
+
+
+end)
+
+
+
+UIS.InputChanged:Connect(function(input)
+
+
+if dragging 
+and input.UserInputType ==
+Enum.UserInputType.MouseMovement then
+
+
+
+local delta =
+input.Position - dragStart
+
+
+
+obj.Position =
+UDim2.new(
+
+startPos.X.Scale,
+
+startPos.X.Offset + delta.X,
+
+startPos.Y.Scale,
+
+startPos.Y.Offset + delta.Y
+
+)
+
+
+
+end
+
+
+
+end)
+
+
+
+end
+
+
+
+
+
+-- kéo menu
+
+MakeDrag(Main)
+
+
+
+-- kéo logo khi ẩn
+
+MakeDrag(Open)
 
 -- SIDEBAR
 
